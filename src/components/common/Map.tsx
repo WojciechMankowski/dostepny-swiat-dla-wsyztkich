d5ca6d5570f25e65fc96c802f916ced365c4b9b5
WojciechMankowski <wojtekm510@gmail.com>
2024-03-28 18:20:43 +0100

    add maps

--- src/components/common/Map.tsx
+++ src/components/common/Map.tsx
@@ -1,66 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { APIProvider, Map, InfoWindow, AdvancedMarker } 
-from "@vis.gl/react-google-maps";
-import fetchCoordinates from "../../helps/get_corordinates";
+import React, { useEffect, useState } from 'react'
+import { APIProvider, Map, InfoWindow, AdvancedMarker } from '@vis.gl/react-google-maps'
+import fetchCoordinates from '../../helps/get_corordinates'
 
 interface MapsProps {
-  addresses: string[];
+	addresses: string[]
 }
 
 interface Coor {
-  lat: number;
-  lng: number;
+	lat: number
+	lng: number
 }
 
 const Maps: React.FC<MapsProps> = ({ addresses }) => {
-  const [markers, setMarkers] = useState<Coor[]>([]);
-  const [selectedMarker, setSelectedMarker] = useState<Coor | null>(null);
+	const [markers, setMarkers] = useState<Coor[]>([])
+	const [selectedMarker, setSelectedMarker] = useState<Coor | null>(null)
 
-  useEffect(() => {
-    const getCoordinates = async () => {
-      const coordinates: Coor[] = await fetchCoordinates(addresses);
-      setMarkers(coordinates);
-    };
+	useEffect(() => {
+		const getCoordinates = async () => {
+			const coordinates: Coor[] = await fetchCoordinates(addresses)
+			setMarkers(coordinates)
+		}
 
-    getCoordinates();
-  }, [addresses]);
+		getCoordinates()
+	}, [addresses])
 
-  const handleMarkerClick = (marker: Coor) => {
-    setSelectedMarker(marker);
-  };
+	const handleMarkerClick = (marker: Coor) => {
+		setSelectedMarker(marker)
+	}
+	const mark = markers.map((marker, index) => (
+		<AdvancedMarker
+			key={index}
+			position={{ lat: marker.lat, lng: marker.lng }}
+			//   onClick={() => handleMarkerClick(marker)}
+		></AdvancedMarker>
+	))
+	return (
+		<APIProvider apiKey="AIzaSyBYDgXMcNfyI1pMmz7dN0228-yGIL9lp8c">
+			<div className="map">
+				<Map zoom={12} center={{ lat: 54.35, lng: 18.6333 }} mapId={'<Your custom MapId here>'}>
+					<AdvancedMarker
+						// className={customMarker}
+						position={{ lat: 53.54992, lng: 10.00678 }}>
+						<h2>I am so customized</h2>
+						<p>That is pretty awesome!</p>
+					</AdvancedMarker>
+					{mark}
+				</Map>
+			</div>
+		</APIProvider>
+	)
+}
 
-  return (
-    <APIProvider apiKey='AIzaSyBYDgXMcNfyI1pMmz7dN0228-yGIL9lp8c'>
-      <div className="map">
-        <Map
-          style={{ width: "100%", height: "400px" }}
-          defaultZoom={10}
-          defaultCenter={{ lat: 54.35,  lng:  18.6333 }}
-          gestureHandling="greedy"
-          disableDefaultUI
-        >
-          {markers.map((marker, index) => (
-            <AdvancedMarker
-              key={index}
-              position={{ lat: marker.lat, lng: marker.lng }}
-              onClick={() => handleMarkerClick(marker)}
-            />
-          ))}
-          {selectedMarker && (
-            <InfoWindow
-              position={{ lat: selectedMarker.lat, lng: selectedMarker.lng }}
-              onCloseClick={() => setSelectedMarker(null)}
-            >
-              <div>
-                <h2>Informacja</h2>
-                <p>Miejsce o współrzędnych: {selectedMarker.lat}, {selectedMarker.lng}</p>
-              </div>
-            </InfoWindow>
-          )}
-        </Map>
-      </div>
-    </APIProvider>
-  );
-};
-
-export default Maps;
+export default Maps
