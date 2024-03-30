3f405610f2fe87562946bccad859bfd96cb9d295
wojciech <wmankowski@gmail.com>
2024-03-30 11:48:17 +0100

    add

--- src/components/common/Img.tsx
+++ src/components/common/Img.tsx
@@ -1,7 +1,6 @@
 import ImgePropsType from "../../types"
 
 const Img = (props: ImgePropsType) => {
-    console.log(props.img)
     return <img src={props.img} alt={props.alt} className="placeImg"/>
 }
 export default Img
